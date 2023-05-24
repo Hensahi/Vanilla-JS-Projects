22d582ca39e44d9d33798cdf97140e84015fab3e
Hensahi <hency.lara@gmail.com>
2023-05-24 19:44:50 -0400

    Se completa el proyecto mmodal implementando las funcionalidades de agregar y remover clases a los botones open modal y close

--- modal-project/js/app.js
+++ modal-project/js/app.js
@@ -3,3 +3,14 @@
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
 
+const MODAL_BTN = document.querySelector(".modal-btn");
+const CLOSE_BTN = document.querySelector(".close-btn");
+const MODAL_OVERLAY = document.querySelector(".modal-overlay");
+
+MODAL_BTN.addEventListener("click", () => {
+    MODAL_OVERLAY.classList.add("open-modal");
+})
+
+CLOSE_BTN.addEventListener("click", () => {
+    MODAL_OVERLAY.classList.remove("open-modal");
+})
